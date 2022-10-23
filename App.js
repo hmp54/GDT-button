7283900ad7fb65698988f84711ccb8dcc87c1920
Helen Pewther <54906116+hmp54@users.noreply.github.com>
2022-10-23 16:29:37 -0400

    Navigation + mysql setup
    Added stack navigation, login + signup screens.  Installed express and mysql libraries to get set up for building the database.

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react'; 
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'; 
-import HomeScreen from './HomeScreen'; 
-import SecondScreen from './SecondScreen';
+import {EventRegister} from 'react-native-event-listeners'; 
+import HomeScreen from './Screens/HomeScreen'; 
+import LoginScreen from './Screens/LoginScreen';
+import SignUpScreen from './Screens/SignUpScreen.js'; 
 
+//creates screen navigation that the app is based on 
 const Stack = createNativeStackNavigator();
 const screenOptions = {
   headerShown: false,
 };
 
-
 export default function App() {
+  {/*//launches database server on initialization of app.
+  //empty array list ensure that this codeo only executes once 
+  const [count, setCount] = useState(0); 
+  useEffect(() =>{
+    const onFocus = () => {
+      console.log('hello world'); 
+    }
+    const focusListener = EventRegister.addEventListener('didFocus', onFocus); 
+    return () => focusListener.remove(); 
+  }, []); */}
+
+  const express = require('express'); 
+  const mysql = require('mysql');
+
+  const app = express(); 
+  app.listen('3000', () =>{ 
+      console.log('server started on port 3000'); 
+  }); 
+
   return (
     <NavigationContainer>
       <Stack.Navigator 
         screenOptions={screenOptions}
         initialRouteName = 'HomeScreen'>
             <Stack.Screen name = 'HomeScreen'component = {HomeScreen}/>
-            <Stack.Screen name = 'SecondScreen' component = {SecondScreen}/>
+            <Stack.Screen name = 'LoginScreen' component = {LoginScreen}/>
+            <Stack.Screen name = 'SignUpScreen' component = {SignUpScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ test.js
@@ -0,0 +1,6 @@
+const express = require('express'); 
+const mysql = require('mysql'); 
+
+test.listen('3000', () =>{
+    console.log('started server on port 3000'); 
+})
